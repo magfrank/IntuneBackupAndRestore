6e9f63ed283d5dfdf38220c850a53b1aacd9e08d
Frank <magnus.frank@storebrand.no>
2025-06-20 14:58:46 +0200

    add workaround to skip and warn about encrypted oma settings if missing read/write

--- IntuneBackupAndRestore/Public/Invoke-IntuneBackupDeviceConfiguration.ps1
+++ IntuneBackupAndRestore/Public/Invoke-IntuneBackupDeviceConfiguration.ps1
@@ -1,5 +1,5 @@
 function Invoke-IntuneBackupDeviceConfiguration {
-    <#
+	<#
     .SYNOPSIS
     Backup Intune Device Configurations
     
@@ -13,23 +13,23 @@ function Invoke-IntuneBackupDeviceConfiguration {
     Invoke-IntuneBackupDeviceConfiguration -Path "C:\temp"
     #>
     
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $true)]
-        [string]$Path,
+	[CmdletBinding()]
+	param(
+		[Parameter(Mandatory = $true)]
+		[string]$Path,
 
-        [Parameter(Mandatory = $false)]
-        [ValidateSet("v1.0", "Beta")]
-        [string]$ApiVersion = "Beta"
-    )
+		[Parameter(Mandatory = $false)]
+		[ValidateSet("v1.0", "Beta")]
+		[string]$ApiVersion = "Beta"
+	)
 
-    #Connect to MS-Graph if required
-    if($null -eq (Get-MgContext)){
-        connect-mggraph -scopes "DeviceManagementApps.Read.All, DeviceManagementConfiguration.Read.All, DeviceManagementServiceConfig.Read.All, DeviceManagementManagedDevices.Read.All" 
-    }
+	#Connect to MS-Graph if required
+	if ($null -eq (Get-MgContext)) {
+		connect-mggraph -scopes "DeviceManagementApps.Read.All, DeviceManagementConfiguration.Read.All, DeviceManagementServiceConfig.Read.All, DeviceManagementManagedDevices.Read.All" 
+	}
 
-    # Get all device configurations
-    $deviceConfigurations = Invoke-MgGraphRequest -Uri "$apiVersion/deviceManagement/deviceConfigurations" | Get-MGGraphAllPages
+	# Get all device configurations
+	$deviceConfigurations = Invoke-MgGraphRequest -Uri "$apiVersion/deviceManagement/deviceConfigurations" | Get-MGGraphAllPages
 
 	if ($deviceConfigurations.value -ne "") {
 
@@ -48,8 +48,29 @@ function Invoke-IntuneBackupDeviceConfiguration {
 				foreach ($omaSetting in $deviceConfiguration.omaSettings) {
 					# Check if this particular setting is encrypted, and get the plaintext only if necessary
 					if ($omaSetting.isEncrypted) {
-						$omaSettingValue = Invoke-MgGraphRequest -Uri "$apiVersion/deviceManagement/deviceConfigurations/$($deviceConfiguration.id)/getOmaSettingPlainTextValue(secretReferenceValueId='$($omaSetting.secretReferenceValueId)')" | Get-MgGraphAllPages
-					} else {
+						# This part is replaced to take into account that we currently only have read access. Readwrite access is needed to do this, because it is regarded potentally highly sensitive data.
+						#$omaSettingValue = Invoke-MgGraphRequest -Uri "$apiVersion/deviceManagement/deviceConfigurations/$($deviceConfiguration.id)/getOmaSettingPlainTextValue(secretReferenceValueId='$($omaSetting.secretReferenceValueId)')" | Get-MgGraphAllPages
+						try {
+							$ErrorActionPreferenceOrig = $ErrorActionPreference
+							$ErrorActionPreference = 'Stop'
+							$omaSettingValue = Invoke-MgGraphRequest -Uri "$apiVersion/deviceManagement/deviceConfigurations/$($deviceConfiguration.id)/getOmaSettingPlainTextValue(secretReferenceValueId='$($omaSetting.secretReferenceValueId)')"  | Get-MgGraphAllPages
+							$ErrorActionPreference = $ErrorActionPreferenceOrig
+						}
+						catch {
+							$ErrorActionPreference = $ErrorActionPreferenceOrig
+							$omaSettingValue = "[[ENCRYPTED VALUE UNREADABLE: missing permission]]"
+							# Build warning message
+							$message = "WARNING: Could not fetch plaintext value for secretReferenceValueId '$($omaSetting.secretReferenceValueId)' in device config '$($deviceConfiguration.displayName)'. Error: $($_.Exception.Message)"
+			
+							# Write to GitHub Actions log
+							Write-Output "::warning::$message"
+			
+							# Also append to a local warnings file in the root path
+							$warningLogPath = Join-Path -Path $Path -ChildPath "IntuneBackupDeviceConfiguration-warnings.txt"
+							Add-Content -Path $warningLogPath -Value $message
+						}
+					}
+					else {
 						$omaSettingValue = $omaSetting.value
 					}
 					# Define a new 'unencrypted' OMA Setting
